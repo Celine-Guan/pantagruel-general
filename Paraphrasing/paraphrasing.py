f736c55abafcb13f0e3755af47d56edcbe880b8f
GUShuyue <gushuyue@hotmail.com>
2025-07-03 12:20:54 +0200

    Update paraphrasing.py

--- Paraphrasing/paraphrasing.py
+++ Paraphrasing/paraphrasing.py
@@ -24,6 +24,7 @@ def set_seed(seed):
     torch.cuda.manual_seed_all(seed)
     torch.backends.cudnn.deterministic = True
     torch.backends.cudnn.benchmark = False
+SEED = 42
 
 """**set up parameters**"""
 
@@ -255,7 +256,7 @@ def run_pawsx():
 
 
 def main():
-    set_seed(42)
+    set_seed(SEED)
     args = parse_args()
     global MODEL_NAME
     MODEL_NAME = args.model_name
