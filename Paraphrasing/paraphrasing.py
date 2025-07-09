b68a5abc8847f70122b56fa34a01acdc706b28bd
GUShuyue <gushuyue@hotmail.com>
2025-07-09 13:48:21 +0200

    Update paraphrasing.py

--- Paraphrasing/paraphrasing.py
+++ Paraphrasing/paraphrasing.py
@@ -24,7 +24,7 @@ def set_seed(seed):
     torch.cuda.manual_seed_all(seed)
     torch.backends.cudnn.deterministic = True
     torch.backends.cudnn.benchmark = False
-SEED = 123
+SEED = 2025
 
 """**set up parameters**"""
 
