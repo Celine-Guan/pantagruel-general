ad61ea3577ee5a355314970a9d1d760756a5dd1b
GUShuyue <gushuyue@hotmail.com>
2025-07-16 16:39:06 +0200

    Update __init__.py

--- Sequence-classification/common/__init__.py
+++ Sequence-classification/common/__init__.py
@@ -1,3 +1,3 @@
-from .model import SequenceClassificationModel
+from .model import SequenceClassificationModel, NLIClassificationModel
 from .utils import set_seed, init_logger, EarlyStopping, load_yaml
 from .training import train_one_epoch, eval_model
